202a06f3d4f68bc1d82b96453dba87676fe43b91
AtharvRedij <atharvaredij@gmail.com>
2020-10-02 19:34:10 +0530

    connected to DB

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const products = require("./data/products");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -21,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
