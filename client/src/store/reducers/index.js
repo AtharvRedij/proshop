95188412dae32bceb8368a2076d51bfb280285df
AtharvRedij <atharvaredij@gmail.com>
2020-10-04 14:23:02 +0530

    shopping cart page & reducer added

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { productList, productDetails } from "./product";
+import { cart } from "./cart";
 
 const reducer = combineReducers({
   productList,
   productDetails,
+  cart,
 });
 
 export default reducer;
