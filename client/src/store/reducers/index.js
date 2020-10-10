7dfec84425ab637b16c39c6292b3f6aa943fbf72
AtharvRedij <atharvaredij@gmail.com>
2020-10-10 14:47:20 +0530

    login page and reducer added

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import { productList, productDetails } from "./product";
 import { cart } from "./cart";
+import { userLogin } from "./user";
 
 const reducer = combineReducers({
   productList,
   productDetails,
   cart,
+  userLogin,
 });
 
 export default reducer;
